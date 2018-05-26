3cb6ff6b2cb4f482dc21cb1729148355e6fb9e74
maheshthippala <mahesh.cubex@gmail.com>
2018-05-27 00:46:17 +0530

    Added Manu , FAB and Snack Bar functionality

--- app/src/main/java/cubex/mahesh/recyclerviewtest/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/recyclerviewtest/MainActivity.kt
@@ -2,8 +2,13 @@ package cubex.mahesh.recyclerviewtest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
         var rview:RecyclerView? = null
@@ -12,9 +17,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         rview = findViewById(R.id.rview1)
         var lManager = LinearLayoutManager(this,
-                LinearLayoutManager.HORIZONTAL,
+                LinearLayoutManager.VERTICAL,
                 false)
         rview?.layoutManager = lManager
         rview?.adapter = MyAdapter(this)
     }
-}
+
+    fun  fab(v:View)
+    {
+    var sBar =    Snackbar.make(v,"Don't Click Me...",
+                Snackbar.LENGTH_INDEFINITE)
+     sBar.setAction("Dismiss", object : View.OnClickListener {
+         override fun onClick(v: View?) {
+             sBar.dismiss()
+         }
+     })
+    sBar.show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean
+    {
+        menuInflater.inflate(R.menu.mymenu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        Toast.makeText(this,
+                item?.title,Toast.LENGTH_LONG).show()
+
+        return super.onOptionsItemSelected(item)
+    }
+
+} // MainActicity
